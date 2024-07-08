af9144a23b60bb3556957ae0b49c8402862399a2
MohitCode17 <mohit.codes17@gmail.com>
2024-07-08 17:53:50 +0530

    add dashboard profile ui

--- client/src/components/DashboardProfile.jsx
+++ client/src/components/DashboardProfile.jsx
@@ -1,5 +1,51 @@
+import { useSelector } from "react-redux";
+import { TextInput, Button } from "flowbite-react";
+
 const DashboardProfile = () => {
-  return <div>DashboardProfile</div>;
+  const { currentUser } = useSelector((state) => state.user);
+
+  return (
+    <div className="max-w-lg mx-auto p-3 w-full">
+      <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
+      <form className="flex flex-col gap-4">
+        <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full">
+          <img
+            src={currentUser.profilePicture}
+            alt="user"
+            className="rounded-full w-full h-full object-cover border-8 dark:border-gray-100 border-gray-500"
+          />
+        </div>
+        <input
+          className="h-10 w-full rounded-md bg-gray-100 px-3 py-2 text-sm focus:outline-none dark:bg-[#111827] dark:text-white"
+          type="text"
+          placeholder="username"
+          value={currentUser.username}
+          id="username"
+        ></input>
+        <input
+          className="h-10 w-full rounded-md bg-gray-100 px-3 py-2 text-sm focus:outline-none dark:bg-[#111827] dark:text-white"
+          type="email"
+          placeholder="email"
+          value={currentUser.email}
+          id="email"
+        ></input>
+        <input
+          className="h-10 w-full rounded-md bg-gray-100 px-3 py-2 text-sm focus:outline-none dark:bg-[#111827] dark:text-white"
+          type="password"
+          placeholder="password"
+          id="password"
+        ></input>
+
+        <Button type="submit" gradientDuoTone="purpleToBlue" outline>
+          Update
+        </Button>
+      </form>
+      <div className="text-red-500 flex justify-between mt-5">
+        <span className="cursor-pointer">Delete Account</span>
+        <span className="cursor-pointer">Sign Out</span>
+      </div>
+    </div>
+  );
 };
 
 export default DashboardProfile;
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import DashboardSidebar from "../components/DashboardSidebar";
+import DashboardProfile from "../components/DashboardProfile";
 
 const Dashboard = () => {
   const location = useLocation();
@@ -22,6 +23,7 @@ const Dashboard = () => {
         <DashboardSidebar />
       </div>
       {/* MAIN */}
+      {tab === "profile" && <DashboardProfile />}
     </div>
   );
 };
